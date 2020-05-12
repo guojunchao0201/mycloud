d859f1bacf125a4a59a95334984a5576325a1bff
guojunchao0201 <859373875@qq.com>
2020-05-12 19:01:55 +0800

    payment

---
+++ cloud-provider-payment8081/src/main/java/com/mycloud/study/payment/CloudProviderPayment8081Application.java
@@ -0,0 +1,13 @@
+package com.mycloud.study.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProviderPayment8081Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8081Application.class, args);
+    }
+
+}
---
+++ cloud-provider-payment8081/src/test/java/com/mycloud/study/payment/CloudProviderPayment8081ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycloud.study.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProviderPayment8081ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
