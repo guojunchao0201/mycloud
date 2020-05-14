19ffe1da165c45fb0e182e21245014a8484428ab
guojunchao0201 <859373875@qq.com>
2020-05-14 22:45:34 +0800

    zookeeper注册节点

---
+++ cloud-provider-payment8084/src/main/java/com/mycloud/study/payment/CloudProviderPayment8084Application.java
@@ -0,0 +1,13 @@
+package com.mycloud.study.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProviderPayment8084Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8084Application.class, args);
+    }
+
+}
