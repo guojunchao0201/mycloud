872d4413d5d8da963d40a9ecf8d1f6dbe118cf0f
guojunchao0201 <859373875@qq.com>
2020-05-15 22:55:19 +0800

    consul节点

---
+++ cloud-consumerzk-order80/src/main/java/com/mycloud/study/consumerzk/CloudConsumerzkOrder80Application.java
@@ -0,0 +1,16 @@
+package com.mycloud.study.consumerzk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 注册进zookeeper
+ */
+@SpringBootApplication
+public class CloudConsumerzkOrder80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerzkOrder80Application.class, args);
+    }
+
+}
---
+++ cloud-provider-payment8086/src/main/java/com/mycloud/study/payment/CloudProviderPayment8086Application.java
@@ -0,0 +1,13 @@
+package com.mycloud.study.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProviderPayment8086Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8086Application.class, args);
+    }
+
+}
