3dd6ff84ce35879a2aa24b838a933855cc8dbde5
guojunchao0201 <859373875@qq.com>
2020-05-19 19:58:49 +0800

    修改pom

--- cloud-provider-payment8084/src/main/java/com/mycloud/study/payment/CloudProviderPayment8084Application.java
+++ cloud-provider-payment8084/src/main/java/com/mycloud/study/payment/CloudProviderPayment8084Application.java
@@ -3,6 +3,9 @@ package com.mycloud.study.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 24513
+ */
 @SpringBootApplication
 public class CloudProviderPayment8084Application {
 
