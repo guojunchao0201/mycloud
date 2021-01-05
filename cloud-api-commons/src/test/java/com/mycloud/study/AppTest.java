5d8b105fe9507d8e769e17a4107e63b0e652b6e2
guojunchao <859373875@qq.com>
2021-01-05 18:06:06 +0800

    、、

--- cloud-api-commons/src/test/java/com/mycloud/study/AppTest.java
+++ cloud-api-commons/src/test/java/com/mycloud/study/AppTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import javax.net.ssl.SSLPeerUnverifiedException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
@@ -16,5 +20,38 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testList() throws InterruptedException {
+        List<Integer> list = new ArrayList<>();
+
+
+        new Thread(() -> {
+            int i = 0;
+            while (true) {
+                list.add(i);
+                i++;
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        Thread.sleep(1000);
+        new Thread(()->{
+            int size = list.size();
+            for (int i = 0; i < size; i++) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(list.get(i));
+            }
+            System.out.println(list.size());
+        }).start();
+        Thread.sleep(999_999_999);
     }
 }
